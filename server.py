e34aaba465e83973bb2e9541058246e6bcadfec3
Gabriel Régis da Cunha <80863853+Regizzera@users.noreply.github.com>
2023-11-29 04:00:42 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,44 +1,92 @@
+from abc import ABC, abstractmethod
 import socket
 import threading
-from recomendacaoKnn import movie_recomendation
+from RecomendacaoKnn import RecomendarFilme # Importa a classe RecomendarFilme
 
-def handle_client(client_socket):
-    try:
-        data = client_socket.recv(1024)
-        movies = data.decode("utf-8").split(',')
+class Recomendador(ABC):
+    @abstractmethod
+    def filmes_recomendados(self, movie):
+        pass
+    
+class GeradorRecomendacao:
+    def __init__(self, recomendador):
+        self.recomendador = recomendador
+    """
+        Classe responsável por gerar recomendações de filmes com base nos dados recebidos.
 
-        print(f"Mensagem recebida: {movies}")
+        Args:
+        - movies: Uma lista de filmes para os quais as recomendações serão geradas.
 
-        recommendations = {}
+        Returns:
+        Um dicionário contendo recomendações para cada filme da lista.
+        """
+    def gerar_recomendacoes(self, movies):
+        recomendacoes = {}
         for movie in movies:
             movie = movie.strip()
-            recommendations[movie] = movie_recomendation(movie)
+            recomendacoes[movie] = self.recomendador.filmes_recomendados(movie)
+        return recomendacoes
 
-        print("Gerando recomendações:", recommendations)
+class ClientHandler:
+    def __init__(self, recomendador):
+        self.recomendador = recomendador
+        """
+        Classe responsável por lidar com a lógica de comunicação com o cliente.
 
-        client_socket.send(str(recommendations).encode("utf-8"))
-    except Exception as e:
-        print("[ERRO] Erro ao lidar com o cliente:", e)
-    finally:
-        client_socket.close()
+        Args:
+        - client_socket: O socket do cliente conectado.
 
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1', 12345))
-    server.listen(5)
+        Funciona recebendo os dados do cliente, gerando recomendações e enviando de volta ao cliente.
+        """
+    def handle(self, client_socket):
+        try:
+            # Recebe os dados do cliente
+            data = client_socket.recv(1024)
+            movies = data.decode("utf-8").split(',')
+            print(f"Mensagem recebida: {movies}")
 
-    print("[INFO] Servidor ouvindo na porta 12345...")
+            # Gera as recomendações com base nos dados recebidos
+            recommendation_generator = GeradorRecomendacao(self.recomendador)
+            recomendacoes = recommendation_generator.gerar_recomendacoes(movies)
+            print("Gerando recomendações:", recomendacoes)
 
-    try:
-        while True:
-            client, addr = server.accept()
-            print("[INFO] Conexão aceita de {}:{}".format(addr[0], addr[1]))
-            client_handler = threading.Thread(target=handle_client, args=(client,))
-            client_handler.start()
-    except KeyboardInterrupt:
-        print("[INFO] Desligando o servidor.")
-    finally:
-        server.close()
+            # Envia as recomendações de volta ao cliente
+            client_socket.send(str(recomendacoes).encode("utf-8"))
+        except Exception as e:
+            print("[ERRO] Erro ao lidar com o cliente:", e)
+        finally:
+            client_socket.close()
+
+class Servidor:
+    def __init__(self, recomendador):
+        self.recomendador = recomendador
+        """
+        Classe responsável por iniciar o servidor e gerenciar as conexões dos clientes.
+        """
+    def start(self):
+        servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        servidor.bind(('127.0.0.1', 12345))
+        servidor.listen(5)
+        print("[INFO] Servidor ouvindo na porta 12345...")
+
+        try:
+            while True:
+                client, addr = servidor.accept()
+                print("[INFO] Conexão aceita de {}:{}".format(addr[0], addr[1]))
+                
+                # Inicia uma nova thread para lidar com o cliente
+                client_handler = threading.Thread(target=ClientHandler(self.recomendador).handle, args=(client,))
+                client_handler.start()
+        except KeyboardInterrupt:
+            print("[INFO] Desligando o servidor.")
+        finally:
+            servidor.close()
+
+# Exemplo de inicialização do Recomendador de Filmes
+recomendador = RecomendarFilme() 
+recomendador.carregar_dados('movies_metadata.csv', 'ratings.csv')
+recomendador.treinar_modelo()
 
 if __name__ == "__main__":
-    start_server()
+    servidor = Servidor(recomendador)
+    servidor.start()
